288a1d6c6fb7dd6d92fce5b7ddc2e8f29bfdc002
Tikhon <tikhon@mail.ru>
2022-10-03 21:41:20 +0300

    add programm for final task

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿
+//Задача: Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+//длина которых меньше либо равна 3 символа. 
+//Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+//При решение не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+/*
+//1-е решение
+using System;
+
+string[] M;
+Console.WriteLine("Введите массив строк через пробел");
+string stroka = Console.ReadLine();
+
+M = stroka.Split(' ');
+var result = new string[M .Length];
+var realSize = 0;
+foreach (var value in M )
+{
+    if (value.Length <= 3)
+    {
+    result[realSize] = value;
+    realSize++;
+    } 
+}
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
+Console.ReadKey(true);
+*/
+
+//2-е решение
+
+using System;
+string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
+string[] array2 = new string[array1.Length];
+
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
